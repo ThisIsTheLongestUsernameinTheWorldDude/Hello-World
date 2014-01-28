5b653011e692ec65da9b18d6082198bf8032a691
ThisIsTheLongestUsernameInTheWorldDude <cpwest11@gmail.com>
2014-01-28 15:32:16 -0600

    efficient

--- io.cc
+++ io.cc
@@ -1,9 +1,10 @@
 #include <iostream>
+using namespace std;
 int main()
 {
-	std::cout << "Enter two numbers." << std::endl;
+	cout << "Enter two numbers." << endl;
 	int v1 = 0, v2 = 0;
-	std::cin >> v1 >> v2;
-	std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std::endl;
+	cin >> v1 >> v2;
+	cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2 << endl;
 	return 0;
 }
